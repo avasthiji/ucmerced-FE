0171cb037fa0970d054c4d1aafb24f55e6d06946
avasthiji <avasthi.d2itechnology@gmail.com>
2024-04-16 00:21:38 +0530

    export api integration

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -5,11 +5,11 @@ import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
   providedIn: 'root'
 })
 export class CalculatorService {
-  BaseURL:string= 'https://eaa0-223-233-66-243.ngrok-free.app/api'
+  BaseURL:string= 'https://ea24-223-233-72-198.ngrok-free.app'
   constructor(private http: HttpClient,) { }
 
   roiCalcuator(data:{}){
-    return this.http.post(`${this.BaseURL}/roiCalculator`,data)
+    return this.http.post(`${this.BaseURL}/api/roiCalculator`,data)
   }
   utilityCost(data:any){
   let params = new HttpParams();
@@ -21,6 +21,37 @@ export class CalculatorService {
   .set('content-type', 'application/json')
   .set('ngrok-skip-browser-warning', 'true')
   .set('Access-Control-Allow-Origin', '*');
-  return this.http.get(`${this.BaseURL}/utilityCost1`, { params: params,headers: headers });
+  return this.http.get(`${this.BaseURL}/api/utilityCost`, { params: params,headers: headers });
   }
+
+  getEthnicity(){
+    return this.http.get(`${this.BaseURL}/api/ethnicity`)
+  }
+  getAgegroup(){
+    return this.http.get(`${this.BaseURL}/api/agegroup`)
+  }
+  getCounties(){
+    return this.http.get(`${this.BaseURL}/api/counties`)
+  }
+  getDiseases(){
+    return this.http.get(`${this.BaseURL}/api/diseases`)
+  }
+  getRegions(){
+    const headers= new HttpHeaders()
+    .set('content-type', 'application/json')
+    .set('ngrok-skip-browser-warning', 'true')
+    .set('Access-Control-Allow-Origin', '*');
+    return this.http.get(`${this.BaseURL}/api/regions`, { headers: headers })
+  }
+  exportTotalCostData(data:any){
+    let params = new HttpParams();
+    Object.keys(data).forEach(key => {
+      params = params.append(key, data[key]);
+    });
+    const headers= new HttpHeaders()
+    return this.http.get(`${this.BaseURL}/api/exportData`,{ params: params,headers: headers })
+  }
+
+
+
 }
--- src/app/services/mockData.ts
+++ src/app/services/mockData.ts
@@ -1,4 +1,565 @@
 export const diseases = {
     production: false
   };
+ 
+  export const diseaseList: any = [
+  {
+      "id": 1,
+      "name": "Arthritis"
+  },
+  {
+      "id": 2,
+      "name": "Asthma"
+  },
+  {
+      "id": 3,
+      "name": "Coronary Heart Disease (CHD)"
+  },
+  {
+      "id": 4,
+      "name": "Congestive Heart Failure (CHF)"
+  },
+  {
+      "id": 5,
+      "name": "Depression"
+  },
+  {
+      "id": 6,
+      "name": "Diabetes"
+  },
+  {
+      "id": 7,
+      "name": "Hypertension"
+  },
+  {
+      "id": 8,
+      "name": "Stroke"
+  },
+  {
+      "id": 9,
+      "name": "Cancer"
+  }
+];
+export const regionList: any = [
+  {
+      "id": 1,
+      "name": "BAY AREA COUNTIES"
+  },
+  {
+      "id": 2,
+      "name": "NORTHERN AND SIERRA COUNTIES"
+  },
+  {
+      "id": 3,
+      "name": "SACRAMENTO AREA COUNTIES"
+  },
+  {
+      "id": 4,
+      "name": "SAN JOAQUIN VALLEY COUNTIES"
+  },
+  {
+      "id": 5,
+      "name": "OTHER SOUTHERN COUNTIES"
+  },
+  {
+      "id": 6,
+      "name": "LOS ANGELES COUNTIES"
+  },
+  {
+      "id": 7,
+      "name": "CENTRAL COAST COUNTIES"
+  }
+];
+export const countiesList: any = [
+  {
+      "id": 1,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "Alameda"
+  },
+  {
+      "id": 2,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Alpine"
+  },
+  {
+      "id": 3,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Amador"
+  },
+  {
+      "id": 4,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Butte"
+  },
+  {
+      "id": 5,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Calaveras"
+  },
+  {
+      "id": 6,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Colusa"
+  },
+  {
+      "id": 7,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "Contra Costa"
+  },
+  {
+      "id": 8,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Del Norte"
+  },
+  {
+      "id": 9,
+      "region": {
+          "id": 3,
+          "name": "SACRAMENTO AREA COUNTIES"
+      },
+      "name": "El Dorado"
+  },
+  {
+      "id": 10,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "Fresno"
+  },
+  {
+      "id": 11,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Glenn"
+  },
+  {
+      "id": 12,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Humboldt"
+  },
+  {
+      "id": 13,
+      "region": {
+          "id": 5,
+          "name": "OTHER SOUTHERN COUNTIES"
+      },
+      "name": "Imperial"
+  },
+  {
+      "id": 14,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Inyo"
+  },
+  {
+      "id": 15,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "Kern"
+  },
+  {
+      "id": 16,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "Kings"
+  },
+  {
+      "id": 17,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Lake"
+  },
+  {
+      "id": 18,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Lassen"
+  },
+  {
+      "id": 19,
+      "region": {
+          "id": 6,
+          "name": "LOS ANGELES COUNTIES"
+      },
+      "name": "Los Angeles"
+  },
+  {
+      "id": 20,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "Madera"
+  },
+  {
+      "id": 21,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "Marin"
+  },
+  {
+      "id": 22,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Mariposa"
+  },
+  {
+      "id": 23,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Mendocino"
+  },
+  {
+      "id": 24,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "Merced"
+  },
+  {
+      "id": 25,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Modoc"
+  },
+  {
+      "id": 26,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Mono"
+  },
+  {
+      "id": 27,
+      "region": {
+          "id": 7,
+          "name": "CENTRAL COAST COUNTIES"
+      },
+      "name": "Monterey"
+  },
+  {
+      "id": 28,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "Napa"
+  },
+  {
+      "id": 29,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Nevada"
+  },
+  {
+      "id": 30,
+      "region": {
+          "id": 5,
+          "name": "OTHER SOUTHERN COUNTIES"
+      },
+      "name": "Orange"
+  },
+  {
+      "id": 31,
+      "region": {
+          "id": 3,
+          "name": "SACRAMENTO AREA COUNTIES"
+      },
+      "name": "Placer"
+  },
+  {
+      "id": 32,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Plumas"
+  },
+  {
+      "id": 33,
+      "region": {
+          "id": 5,
+          "name": "OTHER SOUTHERN COUNTIES"
+      },
+      "name": "Riverside"
+  },
+  {
+      "id": 34,
+      "region": {
+          "id": 3,
+          "name": "SACRAMENTO AREA COUNTIES"
+      },
+      "name": "Sacramento"
+  },
+  {
+      "id": 35,
+      "region": {
+          "id": 7,
+          "name": "CENTRAL COAST COUNTIES"
+      },
+      "name": "San Benito"
+  },
+  {
+      "id": 36,
+      "region": {
+          "id": 5,
+          "name": "OTHER SOUTHERN COUNTIES"
+      },
+      "name": "San Bernardino"
+  },
+  {
+      "id": 37,
+      "region": {
+          "id": 5,
+          "name": "OTHER SOUTHERN COUNTIES"
+      },
+      "name": "San Diego"
+  },
+  {
+      "id": 38,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "San Francisco"
+  },
+  {
+      "id": 39,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "San Joaquin"
+  },
+  {
+      "id": 40,
+      "region": {
+          "id": 7,
+          "name": "CENTRAL COAST COUNTIES"
+      },
+      "name": "San Luis Obispo"
+  },
+  {
+      "id": 41,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "San Mateo"
+  },
+  {
+      "id": 42,
+      "region": {
+          "id": 7,
+          "name": "CENTRAL COAST COUNTIES"
+      },
+      "name": "Santa Barbara"
+  },
+  {
+      "id": 43,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "Santa Clara"
+  },
+  {
+      "id": 44,
+      "region": {
+          "id": 7,
+          "name": "CENTRAL COAST COUNTIES"
+      },
+      "name": "Santa Cruz"
+  },
+  {
+      "id": 45,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Shasta"
+  },
+  {
+      "id": 46,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Sierra"
+  },
+  {
+      "id": 47,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Siskiyou"
+  },
+  {
+      "id": 48,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "Solano"
+  },
+  {
+      "id": 49,
+      "region": {
+          "id": 1,
+          "name": "BAY AREA COUNTIES"
+      },
+      "name": "Sonoma"
+  },
+  {
+      "id": 50,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "Stanislaus"
+  },
+  {
+      "id": 51,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Sutter"
+  },
+  {
+      "id": 52,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Tehama"
+  },
+  {
+      "id": 53,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Trinity"
+  },
+  {
+      "id": 54,
+      "region": {
+          "id": 4,
+          "name": "SAN JOAQUIN VALLEY COUNTIES"
+      },
+      "name": "Tulare"
+  },
+  {
+      "id": 55,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Tuolomne"
+  },
+  {
+      "id": 56,
+      "region": {
+          "id": 7,
+          "name": "CENTRAL COAST COUNTIES"
+      },
+      "name": "Ventura"
+  },
+  {
+      "id": 57,
+      "region": {
+          "id": 3,
+          "name": "SACRAMENTO AREA COUNTIES"
+      },
+      "name": "Yolo"
+  },
+  {
+      "id": 58,
+      "region": {
+          "id": 2,
+          "name": "NORTHERN AND SIERRA COUNTIES"
+      },
+      "name": "Yuba"
+  }
+];
+export const ethnicityList: any = [
+  {
+      "id": 1,
+      "name": "African American"
+  },
+  {
+      "id": 2,
+      "name": "ASIAN"
+  },
+  {
+      "id": 3,
+      "name": "HISPANIC"
+  },
+  {
+      "id": 4,
+      "name": "Other Races"
+  },
+  {
+      "id": 5,
+      "name": "Non-Hispanic White"
+  },
+  {
+      "id": 6,
+      "name": "White/Other"
+  }
+];
   
