4bab36cee9d383e03443fcbedfbc5d0d726312c7
avasthiji <avasthi.d2itechnology@gmail.com>
2024-04-27 16:00:41 +0530

    ui-responsive

--- src/app/components/chronic-disease/roi-results/roi-results.component.ts
+++ src/app/components/chronic-disease/roi-results/roi-results.component.ts
@@ -12,44 +12,7 @@ export class RoiResultsComponent implements OnInit {
 
   ngOnInit(): void {
   }
- 
-downloadCSV(fileName: string) {
-  const tableElement = document.getElementById('roiTable');
-  
-  if (tableElement) {
-      const table = tableElement as HTMLTableElement;
 
-      const csvData: string[] = [];
-
-      Array.from(table.rows).forEach(row => {
-          const csvRow: string[] = [];
-          Array.from(row.cells).forEach(cell => {
-              const cellData = cell.textContent ? cell.textContent.replace(/"/g, '""') : '';
-              csvRow.push(`"${cellData}"`);
-          });
-          csvData.push(csvRow.join(','));
-      });
-
-      const csvContent = csvData.join('\n');
-
-      const blob = new Blob([csvContent], { type: 'text/csv' });
-
-      const url = URL.createObjectURL(blob);
-
-      const link = document.createElement('a');
-      link.href = url;
-      link.download = `${fileName}.csv`;
-
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      URL.revokeObjectURL(url);
-      
-  } else {
-      console.error('Table element with ID "roiTable" not found.');
-  }
-}
 
 
 }
