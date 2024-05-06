209ba70272d312518b5631e0fa4bf41db4dd5420
avasthiji <avasthi.d2itechnology@gmail.com>
2024-05-06 06:52:58 +0530

    environment variable

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -6,56 +6,50 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class CalculatorService {
-  BaseURL:string= environment.BASEURL
+  BaseURL: string = environment.BASEURL
   headers = new HttpHeaders()
-  .set('content-type', 'application/json')
-  .set('ngrok-skip-browser-warning', 'true')
-  .set('Access-Control-Allow-Origin', '*');
+    .set('content-type', 'application/json')
+    .set('ngrok-skip-browser-warning', 'true')
+    .set('Access-Control-Allow-Origin', '*');
   constructor(private http: HttpClient,) { }
 
-  roiCalcuator(data:{}){
-    const headers= new HttpHeaders()
-  .set('content-type', 'application/json')
-  .set('ngrok-skip-browser-warning', 'true')
-  .set('Access-Control-Allow-Origin', '*');
-    return this.http.post(`${this.BaseURL}/api/roiCalculator`,data)
+  roiCalcuator(data: {}) {
+    const headers = new HttpHeaders()
+      .set('content-type', 'application/json')
+      .set('ngrok-skip-browser-warning', 'true')
+      .set('Access-Control-Allow-Origin', '*');
+    return this.http.post(`${this.BaseURL}/api/roiCalculator`, data)
   }
-  utilityCost(data:any){
-  let params = new HttpParams();
-  Object.keys(data).forEach(key => {
-    params = params.append(key, data[key]);
-  });
-
-  //const headers = new HttpHeaders().set('ngrok-skip-browser-warning', '96420');
-  // const headers= new HttpHeaders()
-  // .set('content-type', 'application/json')
-  // .set('ngrok-skip-browser-warning', 'true')
-  // .set('Access-Control-Allow-Origin', '*');
-  return this.http.get(`${this.BaseURL}/api/utilityCost`, { params: params,headers: this.headers });
-  }
-
-  getEthnicity(){
-    return this.http.get(`${this.BaseURL}/api/ethnicity`,{ headers: this.headers })
-  }
-  getAgegroup(){
-    return this.http.get(`${this.BaseURL}/api/agegroup`,{ headers: this.headers })
-  }
-  getCounties(){
-    return this.http.get(`${this.BaseURL}/api/counties`,{ headers: this.headers })
-  }
-  getDiseases(){
-    return this.http.get(`${this.BaseURL}/api/diseases`,{ headers: this.headers })
-  }
-  getRegions(){
-    return this.http.get(`${this.BaseURL}/api/regions`, { headers: this.headers })
-  }
-  exportTotalCostData(data:any){
+  utilityCost(data: any) {
     let params = new HttpParams();
     Object.keys(data).forEach(key => {
       params = params.append(key, data[key]);
     });
-    const headers= new HttpHeaders()
-    return this.http.get(`${this.BaseURL}/api/exportData`,{ params: params,headers: headers })
+    return this.http.get(`${this.BaseURL}/api/utilityCost`, { params: params, headers: this.headers });
+  }
+  exportTotalCostData(data: any) {
+    let params = new HttpParams();
+    Object.keys(data).forEach(key => {
+      params = params.append(key, data[key]);
+    });
+    const headers = new HttpHeaders()
+    return this.http.get(`${this.BaseURL}/api/exportData`, { params: params, headers: headers })
+  }
+
+  getEthnicity() {
+    return this.http.get(`${this.BaseURL}/api/ethnicity`, { headers: this.headers })
+  }
+  getAgegroup() {
+    return this.http.get(`${this.BaseURL}/api/agegroup`, { headers: this.headers })
+  }
+  getCounties() {
+    return this.http.get(`${this.BaseURL}/api/counties`, { headers: this.headers })
+  }
+  getDiseases() {
+    return this.http.get(`${this.BaseURL}/api/diseases`, { headers: this.headers })
+  }
+  getRegions() {
+    return this.http.get(`${this.BaseURL}/api/regions`, { headers: this.headers })
   }
 
 
