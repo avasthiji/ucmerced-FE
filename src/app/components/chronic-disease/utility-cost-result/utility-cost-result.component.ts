a2ad6f7ebfab16b1b18a885e643f76c9a8159f5d
avasthiji <avasthi.d2itechnology@gmail.com>
2024-05-29 12:09:25 +0530

    export table update

--- src/app/components/chronic-disease/utility-cost-result/utility-cost-result.component.ts
+++ src/app/components/chronic-disease/utility-cost-result/utility-cost-result.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Workbook } from 'exceljs';
 import { CsvExportServiceService } from 'src/app/services/csv-export-service.service';
-
+import * as fs from 'file-saver';
 @Component({
   selector: 'app-utility-cost-result',
   templateUrl: './utility-cost-result.component.html',
@@ -15,12 +16,51 @@ export class UtilityCostResultComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  downloadCSV() {
-    const table = document.getElementById('totalCostTable');
-    if (table) {
-      this.csvExportService.exportTableToCsv(table, 'table_data');
-    } else {
-      console.error('Table element not found.');
-    }
-    }
+  // downloadCSV() {
+  //   const table = document.getElementById('totalCostTable');
+  //   if (table) {
+  //     this.csvExportService.exportTableToCsv(table, 'table_data');
+  //   } else {
+  //     console.error('Table element not found.');
+  //   }
+  //   }
+    downloadCSV(): void {
+      let workbook = new Workbook();
+          const worksheet = workbook.addWorksheet('Total Cost');
+          worksheet.addRow(['Conditon(s)','County','Cost per case','Utility loss per case','Rates','Cases','Utility Loss','Total Utility Cost','Health Care Costs', 'Total Cost']);
+
+          worksheet.addRow([
+          this.costs['Counties'].conditions,
+          this.costs['Counties'].county,
+          this.costs['Counties'].costPerCase,
+          this.costs['Counties'].utilityLossPerCase,
+          this.costs['Counties'].rates,
+          this.costs['Counties'].cases,
+          this.costs['Counties'].utilityLoss,
+          this.costs['Counties'].costOfUtility,
+          this.costs['Counties'].healthCareCost,
+          this.costs['Counties'].totalCost,
+          ])
+          worksheet.addRow([
+          this.costs['Totals'].conditions,
+          this.costs['Totals'].county,
+          this.costs['Totals'].costPerCase,
+          this.costs['Totals'].utilityLossPerCase,
+          this.costs['Totals'].rates,
+          this.costs['Totals'].cases,
+          this.costs['Totals'].utilityLoss,
+          this.costs['Totals'].costOfUtility,
+          this.costs['Totals'].healthCareCost,
+          this.costs['Totals'].totalCost,
+          ])
+      
+      workbook.xlsx.writeBuffer().then((data) => {
+          let blob = new Blob([data], {
+            type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+          });
+          fs.saveAs(blob, 'total_Data');
+        });
+
+  }
+
 }
